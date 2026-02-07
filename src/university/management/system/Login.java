82d820b1a942da29424d19c92785f2ae60cd5c06
SatyaPrasannaTadi <157570446+SatyaPrasannaTadi@users.noreply.github.com>
2026-02-07 16:17:28 +0530

    Add files via upload

---
+++ src/university/management/system/Login.java
@@ -0,0 +1,106 @@
+package university.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+
+public class Login extends JFrame implements ActionListener {
+
+    JButton login, cancel;
+    JTextField tfusername, tfpassword;
+
+    Login() {
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+
+        JLabel lblusername = new JLabel("Username");
+        lblusername.setBounds(40, 20, 100, 20);
+        add(lblusername);
+
+        tfusername = new JTextField();
+        tfusername.setBounds(150, 20, 150, 20);
+        add(tfusername);
+
+        JLabel lblpassword = new JLabel("Password");
+        lblpassword.setBounds(40, 70, 100, 20);
+        add(lblpassword);
+
+        tfpassword = new JPasswordField();
+        tfpassword.setBounds(150, 70, 150, 20);
+        add(tfpassword);
+
+        login = new JButton("Login");
+        login.setBounds(40, 140, 120, 30);
+        login.setBackground(Color.BLACK);
+        login.setForeground(Color.WHITE);
+        login.addActionListener(this);
+        login.setFont(new Font("Tahoma", Font.BOLD, 15));
+        add(login);
+
+        cancel = new JButton("Cancel");
+        cancel.setBounds(180, 140, 120, 30);
+        cancel.setBackground(Color.BLACK);
+        cancel.setForeground(Color.WHITE);
+        cancel.addActionListener(this);
+        cancel.setFont(new Font("Tahoma", Font.BOLD, 15));
+        add(cancel);
+
+        // SAFE IMAGE LOADING CODE
+        try {
+            java.net.URL imgURL = ClassLoader.getSystemResource("icons/second.jpg");
+
+            if (imgURL != null) {
+                ImageIcon i1 = new ImageIcon(imgURL);
+                Image i2 = i1.getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT);
+                ImageIcon i3 = new ImageIcon(i2);
+                JLabel image = new JLabel(i3);
+                image.setBounds(350, 0, 200, 200);
+                add(image);
+            } else {
+                System.out.println("Login image not found!");
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error loading login image: " + e);
+        }
+
+        setSize(600, 300);
+        setLocation(500, 250);
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == login) {
+
+            String username = tfusername.getText();
+            String password = tfpassword.getText();
+
+            String query = "select * from login where username='" + username + "' and password='" + password + "'";
+
+            try {
+                Conn c = new Conn();
+                ResultSet rs = c.s.executeQuery(query);
+
+                if (rs.next()) {
+                    setVisible(false);
+                    new Project();
+                } else {
+                    JOptionPane.showMessageDialog(null, "Invalid username or password");
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } else if (ae.getSource() == cancel) {
+            System.exit(0);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Login();
+    }
+}
